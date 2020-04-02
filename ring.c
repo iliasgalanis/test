da6ac34d32c826dd52adbf2ed3e9d33d2fadb339
Galanis <gill@localhost.localdomain>
2020-04-02 21:06:01 +0300

    Commited to test Branch

--- ring.c
+++ ring.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
    if(rank==0){
-      printf("\nRank:%2d, stelnw: %4d\n", rank, a);
+      printf("\nRank:%2d, stelnw twra: %4d\n", rank, a);
       MPI_Send(&a,1,MPI_INT,1,0,MPI_COMM_WORLD);
       MPI_Recv(&a,1,MPI_INT,size-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
       printf("\nRank:%2d, parelava: %4d\n\n", rank, a);
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
    else 
    {
       MPI_Recv(&a,1,MPI_INT,(rank-1),0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-      printf("\nRank:%2d, parelava: %4d", rank, a);
+      printf("\nRank:%2d, parelava xtes: %4d", rank, a);
       a*=2;
       printf("\nRank:%2d, stelnw: %4d\n", rank, a);
       MPI_Send(&a,1,MPI_INT,(rank+1)%size,0,MPI_COMM_WORLD);
